e255dc1650e091816740db8720440e307c987955
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-12-06 09:16:24 +0300

    added ti insert.sql

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate',23, 'M', 'black', 'kind',1),
+('Rowdy',25, 'M', 'black', 'rude',1),
+('Tabitha',19, 'F', 'brown', 'assmatic',1),
+('Sergeant Brown',14, 'M', 'brown', 'agressive',0),
+('Melissa',20, 'F', 'brown', 'lovely',1),
+('Grinch',77, 'M', 'green', 'polite',1),
+('Wendy',23, 'F', 'brown', 'assmatic',1),
+(NULL,23, 'F', 'brown', 'unknown',1);
