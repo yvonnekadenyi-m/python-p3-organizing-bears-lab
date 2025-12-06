7bf00b9953565fccf083780ac3bc06bcddd8865e
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-12-06 09:17:15 +0300

    added ti insert.sql

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,38 @@
+
+# SELECT all female bears (name + age)
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+    SELECT name, age
+    FROM bears
+    WHERE sex = 'F';
 """
 
+# SELECT all bear names alphabetically
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+    SELECT name
+    FROM bears
+    ORDER BY name ASC;
 """
 
+# SELECT alive bears (name + age) sorted youngest → oldest
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+    SELECT name, age
+    FROM bears
+    WHERE alive = 1
+    ORDER BY age ASC;
 """
 
+# SELECT oldest bear
 select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT name, age
+    FROM bears
+    ORDER BY age DESC
+    LIMIT 1;
 """
+
+# SELECT youngest bear
 select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT name, age
+    FROM bears
+    ORDER BY age ASC
+    LIMIT 1;
 """
